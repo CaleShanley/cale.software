0e7816836dc6000f10920d4ff9dc21b7b224139b
Jay McMullen <jay@safewill.com>
2020-08-06 21:21:41 +1000

    Refactor hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,48 +1,52 @@
-import PropTypes from 'prop-types'
-import React from 'react'
-import styled from 'styled-components'
-import { Container } from './layoutComponents'
-import { ReactComponent as Githublogo } from '../images/svg/githublogo.svg'
-import { ReactComponent as Linkedinlogo } from '../images/svg/linkedinlogo.svg'
-import Headshot from './headshot'
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from './layoutComponents';
+import { ReactComponent as Githublogo } from '../images/svg/githublogo.svg';
+import { ReactComponent as Linkedinlogo } from '../images/svg/linkedinlogo.svg';
+import Headshot from './headshot';
 
-const Hero = ({ siteTitle, className }) => (
-  <div className={className}>
-    <Container>
-      <HeroContainer>
-        <Headshot />
-        <Paragraph>
-          <h1>I'm a Junior Web developer from Sydney.</h1>
-          Over the past 4 months, I've gained a ton of experience studying
-          through General Assembly's Software Engineering immersive course. I'm
-          passionate, motivated and excited for my future in the industry.
-          <div class="devicon-wrapper">
-            <div class="githublogo">
-              <a href="https://github.com/CaleShanley">
-                <Githublogo />
-              </a>
-            </div>
-            <div class="linkedinlogo">
-              <a href="https://www.linkedin.com/in/caleshanley/">
-                <Linkedinlogo />
-              </a>
-            </div>
-          </div>
-        </Paragraph>
-      </HeroContainer>
-    </Container>
-  </div>
-)
+const Hero = () => (
+  <HeroContainer>
+    <Headshot />
+    <Paragraph>
+      <Title>I'm a Junior Web developer from Sydney.</Title>
+      <p>
+        Over the past 4 months, I've gained a ton of experience studying through
+        General Assembly's Software Engineering immersive course. I'm
+        passionate, motivated and excited for my future in the industry.
+      </p>
+      <IconWrapper>
+        <a href="https://github.com/CaleShanley" target="_blank" rel="nofollow">
+          <Githublogo />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/caleshanley/"
+          target="_blank"
+          rel="nofollow"
+        >
+          <Linkedinlogo />
+        </a>
+      </IconWrapper>
+    </Paragraph>
+  </HeroContainer>
+);
 
-Hero.propTypes = {
-  siteTitle: PropTypes.string
-}
+const Title = styled.h1`
+  margin: 1.5rem 0;
+  color: black;
+  font-size: 45px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
 
-Hero.defaultProps = {
-  siteTitle: ``
-}
+const IconWrapper = styled.div`
+  display: flex;
+  max-width: 2rem;
+  margin-top: 2rem;
+`;
 
-const StyledHero = styled(Hero)`
+const HeroContainer = styled.div`
   background: var(--white);
   padding: 1rem 0;
   margin-bottom: 3rem;
@@ -52,34 +56,13 @@ const StyledHero = styled(Hero)`
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
-  h1 {
-    margin: 0;
-    margin-top: 1.5em;
-    margin-bottom: 1rem;
-    color: black;
-    font-size: 45px;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  .devicon-wrapper {
-    max-width: 2rem;
-    margin-top: 2rem;
-  }
-
-  .githublogo {
-    margin-bottom: 1rem;
-  }
-`
-
-const HeroContainer = styled.div`
   display: grid;
   grid-template-columns: 1.3fr 2fr;
   gap: 5rem;
-`
+`;
 
 const Paragraph = styled.p`
   color: black;
-`
+`;
 
-export default StyledHero
+export default Hero;
