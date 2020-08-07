225fda838170ffc9d2e78aa5d29e44dbfaf2ed49
CaleShanleyy <CaleShanley1@outlook.com>
2020-08-07 12:23:38 +1000

    media query

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,18 +4,18 @@ const sizes = {
   giant: 1170,
   desktop: 992,
   tablet: 768,
-  phone: 376,
-}
+  phone: 376
+};
 
 // Iterate through the sizes and create a media template
 export const media = Object.keys(sizes).reduce((accumulator, label) => {
   // Use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-  const emSize = sizes[label] / 16
+  const emSize = sizes[label] / 16;
   accumulator[label] = (...args) => css`
     @media (min-width: ${emSize}em) {
       ${css(...args)};
     }
-  `
-  return accumulator
-}, {})
+  `;
+  return accumulator;
+}, {});
