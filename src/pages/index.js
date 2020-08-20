664de14eafb9b7dbde5b1db54d54841fb11aebca
CaleShanleyy <CaleShanley1@outlook.com>
2020-08-20 13:59:04 +1000

    meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
-import React from 'react'
-
-import Layout from '../components/layout'
-import Hero from '../components/hero'
-import Languages from '../components/languages'
-import Portfolio from '../components/portfolio'
-import { Container } from '../components/layoutComponents'
-import SEO from '../components/seo'
+import React from 'react';
+import MetaTags from 'react-meta-tags';
+import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Languages from '../components/languages';
+import Portfolio from '../components/portfolio';
+import { Container } from '../components/layoutComponents';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <Container>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />\
+      <meta property="og:image" content="src/images/portfolio.png" />
       <Hero />
       <Languages />
       <Portfolio />
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     </Container>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
