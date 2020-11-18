817164dd2e7f4374ecf22e2bcd8f534de3b25a5e
CaleShanleyy <CaleShanley1@outlook.com>
2020-11-18 16:48:28 +1100

    meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,17 +12,17 @@ const IndexPage = () => (
   <Layout>
     <Container>
       <Helmet>
-        <title>Cale's portfolio</title>
-        <meta name="title" content="Cale's portfolio" />
+        <title>Cale's Portfolio</title>
+        <meta name="title" content="Cale's Portfolio" />
         <meta name="description" content="Junior Website Developer" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://cale.software" />
-        <meta property="og:title" content="Cale's portfolio" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Cale's Portfolio" />
         <meta property="og:description" content="Junior Website Developer" />
         <meta
           property="og:image"
-          content="src/images/cale_headshot_b_w-removebg-preview (1) (8).png"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
         />
       </Helmet>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
