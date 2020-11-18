99c515d332fb00bff3d855d12452072b83778ad8
CaleShanleyy <CaleShanley1@outlook.com>
2020-11-18 17:33:59 +1100

    meta image

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
         <meta property="og:url" content="https://cale.software" />
         <meta property="og:title" content="Cale's Portfolio" />
         <meta property="og:description" content="Junior Website Developer" />
-        <meta property="og:image" content="/src/images/portfolio-meta.png" />
+        <meta property="og:image" content="https://cale.software" />
       </Helmet>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <Hero />
