20a50b8abaa9a2fe531280cc6e5681998f28af7a
CaleShanleyy <CaleShanley1@outlook.com>
2020-11-18 16:32:49 +1100

    meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,26 @@ import Hero from '../components/hero';
 import Languages from '../components/languages';
 import Portfolio from '../components/portfolio';
 import { Container } from '../components/layoutComponents';
+import { Helmet } from 'react-helmet';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <Container>
+      <Helmet>
+        <title>Cale's portfolio</title>
+        <meta name="title" content="Cale's portfolio" />
+        <meta name="description" content="Junior Website Developer" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://cale.software" />
+        <meta property="og:title" content="Cale's portfolio" />
+        <meta property="og:description" content="Junior Website Developer" />
+        <meta
+          property="og:image"
+          content="src/images/cale_headshot_b_w-removebg-preview (1) (8).png"
+        />
+      </Helmet>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <Hero />
       <Languages />
